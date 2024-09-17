b820333af776f28a660ed668c8f45b12610b8dca
Unis Mohammed <adanunis@gmail.com>
2024-09-17 11:09:23 +0100

    completed fizzbuzz challenge

--- conditionals.js
+++ conditionals.js
@@ -41,6 +41,32 @@ const isValidLength = (phoneNumber) => {
 // colleague was missing the strict equality comparison operator on line 35.
 // also the colleague left out the keyword 'return' in lines 36 and 38
 // leaving the return value is 'undefined'.
-module.exports = isValidLength
+// module.exports = isValidLength
 
 
+// == CHALLENGE ==
+
+// This is a process feedback challenge: please do a screen recording of yourself working on it and submit the file. The relevant Aptem assignment is called Learning a New Language.
+
+// Write a fizzBuzz function in JavaScript. As a reminder, this function should take a number as argument, and then either:
+
+// Return "Fizz" if this number is divisible by 3.
+// Return "Buzz" if this number is divisible by 5.
+// Return "FizzBuzz" if this number is divisible by both 5 and 3.
+// Otherwise, Return the number itself.
+
+const fizzBuzz = (num) => {
+    if (num <= 0) {
+        return num;
+    } else if (num % 15 === 0) {
+        return 'FizzBuzz';
+    } else if (num % 3 === 0) {
+        return 'Fizz';
+    } else if (num % 5 === 0) {
+        return 'Buzz';
+    } else {
+        return num;
+    }
+}
+
+module.exports = fizzBuzz
