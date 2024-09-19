52d376d1a347037095bd33e9cf9979aff8efc97a
Unis Mohammed <adanunis@gmail.com>
2024-09-19 13:11:30 +0100

    completed the ShoppingBasket and candy challenge

--- candy.js
+++ candy.js
@@ -23,3 +23,40 @@
 
 // > basket.getTotalPrice();
 // 12.97
+
+class Candy {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getPrice() {
+        return this.price;
+    }
+}
+
+
+class ShoppingBasket {
+    constructor() {
+        this.basket = [];
+    }
+
+    addItem(candy) {
+        return this.basket = this.basket.concat(candy);
+    }
+
+    getTotalPrice() {
+        //['Mars': 1.99, 'Snickers': 2.50, 'Bounty': 1.20] this.basket
+        //[1.99, 2.50, 1.20] prices only
+        return this.basket.map(bar => bar.getPrice()).reduce((total, price) => {
+            return total + price;
+        }, 0);
+    }
+}
+
+
+module.exports = {Candy, ShoppingBasket}
